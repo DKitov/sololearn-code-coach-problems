28fb85207ddd0f382f48fa964b643897c95c97a3
DKitov <D.kitov7@gmail.com>
2023-08-25 19:58:28 +0300

    sololearn code coach problems

---
+++ popsycles.py
@@ -0,0 +1,10 @@
+siblings = int(input())
+popsicles = int(input())
+# check if you can give even number of popsicles to each sibling
+
+if popsicles % siblings == 0:
+    print('give away')
+# else eat the popsicles by yourself
+
+else:
+    print('eat them yourself')
